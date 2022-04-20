78a3dafd3b4f8bce4e7469f658b8ad029b6a966b
Victor323232 <victorhugoabc59@gmail.com>
2022-04-20 11:14:05 -0300

    Start Commit

---
+++ lib/calculo.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class calculo extends StatefulWidget {
+  const calculo({Key? key}) : super(key: key);
+
+  @override
+  State<calculo> createState() => _calculoState();
+}
+
+class _calculoState extends State<calculo> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Álcool ou Gasolina"),
+        backgroundColor: Color(0xfff520e8e),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20),
+        child: Column (
+          children: <Widget>[
+          Padding(padding:EdgeInsets.only(bottom: 10),
+            child:Image.asset('images/logo.png'),
+          ),
+            Padding(padding: EdgeInsets.all(18),
+              child:Text(
+                "Saiba qual a melhor opção para abastecimento do seu carro",
+                style:TextStyle(
+                  fontSize: 27,
+                  fontWeight: FontWeight.bold,
+                ),
+
+              ),
+            ),
+           TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                    labelText: "Preço Álcool ex:1.59",
+                ),
+                style: TextStyle(
+                    fontSize: 20
+                ),
+
+              ),
+             TextField(
+                keyboardType: TextInputType.number,
+                decoration: InputDecoration(
+                  labelText: "Preço Gasolina ex:3.59",
+                ),
+                style: TextStyle(
+                    fontSize: 20
+                ),
+              ),
+
+           Padding(padding: EdgeInsets.all(20),
+              child: ElevatedButton(
+                 child: Text("Calcular",
+                   style: TextStyle(
+                     fontSize: 30,
+                   ),
+                 ),
+                 onPressed: (){},
+               )
+
+           )
+          ],
+        ),
+
+      ),
+    );
+  }
+}
